d54e8fa88235b681363edb3c74692b07cde631c5
FedericoCaf <fedetheshield09@gmail.com>
2021-11-24 16:58:10 +0100

    header-in-progress

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,29 @@
 <template>
-  <div id="app">
- 
+  <div>
+     <Header/>
+    <main>MAIN</main>
+    <section>SECTION</section>
+    <footer>FOOTER</footer>
   </div>
 </template>
 
 <script>
 
+import Header from './components/Header.vue';
+
 
 export default {
   name: 'App',
   components: {
-   
+   Header,
   }
 }
 </script>
 
 <style lang="scss">
+
+  @import "./assets/style/vars.scss";
+  @import "./assets/style/generals.scss";
+  @import "./assets/style/utilities.scss";
 
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,127 @@
+<template>
+  <header>
+    <div class="fc-container fc-flex-between">
+       <div class="logo-header">
+         <a href="#">
+           <img src="../assets/img/dc-logo.png" alt="logo">
+         </a>
+       </div>
+       <div class="menu-header">
+         <nav>
+            <ul class="fc-flex-between-align-center">
+              <li :class="{'active-border': item.current}" class="fc-flex-align-center" v-for="(item, index) in links" :key="index">
+                 <a :class="{'active-text': item.current}" :href="item.url">{{item.text}}</a>
+              </li>
+            </ul>
+        </nav>
+       </div>
+    </div>
+   
+  </header>
+</template>
+
+<script>
+export default {
+ name: 'Header',
+
+ data(){
+   return{
+     links:[
+        {
+          url:'/CHARACTERS',
+          text: 'CHARACTERS',
+          current: false
+        },
+        {
+          url:'/COMICS',
+          text: 'COMICS',
+          current: true
+        },
+        {
+          url:'/MOVIES',
+          text: 'MOVIES',
+          current: false
+        },
+        {
+          url:'/TV',
+          text: 'TV',
+          current: false
+        },
+        {
+          url:'/GAMES',
+          text: 'GAMES',
+          current: false
+        },
+        {
+          url:'/COLLECTIBLES',
+          text: 'COLLECTIBLES',
+          current: false
+        },
+        {
+          url:'/VIDEOS',
+          text: 'VIDEOS',
+          current: false
+        },
+        {
+          url:'/FANS',
+          text: 'FANS',
+          current: false
+        },
+        {
+          url:'/NEWS',
+          text: 'NEWS',
+          current: false
+        },
+        {
+          url:'/SHOP',
+          text: 'SHOP',
+          current: false
+        },
+      ]
+   }
+ }
+
+
+}
+
+</script>
+
+<style lang="scss" scoped>
+
+@import '../assets/style/vars.scss';
+@import '../assets/style/utilities.scss';
+
+  header{
+    height: 100px;
+    background-color: aqua;
+  }
+
+  .logo-header{
+    height: 100px;
+    width: 100px;
+    padding: 15px;
+  }
+
+  .menu-header{
+    height: 100px;
+    width: 700px;
+  }
+
+  .active-text{
+    color: $primary-color !important;
+  }
+
+  .active-border{
+    border-bottom: 2px solid $primary-color;
+    margin-bottom: -2px;
+  }
+
+  li{
+    height: 100px;
+     a{
+       color: black;
+     }
+  }
+
+
+</style>
